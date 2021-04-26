a0fd7e986286d737616c1da3547ccb0600b59938
c20juler <c20juler@student.his.se>
2021-04-26 11:11:15 +0200

    Added listview in layout and two private member variables in MainActivity.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Mountain> items;
+    private ArrayAdapter<Mountain> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
